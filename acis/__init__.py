f5f1a63c954f58893ede1c66c947daef8d386cb3
John ZuHone <jzuhone@gmail.com>
2016-01-08 00:13:35 -0500

    Adding classes for grabbing ACE data

--- acis/__init__.py
+++ acis/__init__.py
@@ -1,3 +1,4 @@
 from acis.load_review import LoadReview, LoadReviewObscat
 from acis.obscat import Obscat
 from acis.states import TemperatureModel, ACISStates
+from acis.weather import ACEParticles, ACESolarWind, ACEMagneticField
