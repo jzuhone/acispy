9e6b0ceb4d0218303791eb85580beb2ab059a4df
John ZuHone <jzuhone@gmail.com>
2016-03-30 11:31:35 -0400

    Get rid of stuff we're not using

--- acis/__init__.py
+++ acis/__init__.py
@@ -1,5 +1,3 @@
-from acis.load_review import LoadReview, LoadReviewObscat
-from acis.obscat import Obscat
 from acis.msids import MSIDs
 from acis.states import States
 from acis.plots import DatePlot, MultiDatePlot
