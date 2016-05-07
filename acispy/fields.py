Fixing up averages and interpolation

acispy/fields.py
@@ -1,7 +1,8 @@
 import numpy as np
+import Ska.Numpy
 from acispy.utils import moving_average, \
-    get_display_name, interpolate, \
-    unit_table
+    get_display_name, unit_table
+import astropy.units as apu
 
 derived_fields = {}
 
@@ -41,7 +42,7 @@ def add_derived_field(type, name, function, deps, units, time_func=None,
 
 def add_averaged_field(type, name, n=5):
     def _avg(dc):
-        return moving_average(dc[type, name], n=n)*dc[type, name].unit
+        return moving_average(dc[type, name], n=n)
     def _avg_times(dc):
         return dc.times(type, name)[(n-1)/2:(-n+1)/2]
     display_name = "Average %s" % get_display_name(type, name)
@@ -50,13 +51,14 @@ def add_averaged_field(type, name, n=5):
                       time_func=_avg_times, display_name=display_name)
 
 def add_interpolated_field(type, name, times):
+    times_out = np.array(times)
+    units = unit_table[type].get(name, '')
     def _interp(dc):
         times_in = dc.times(type, name).value
-        ok, idxs = interpolate(times_in, times)
-        return dc[type, name][idxs]
+        return Ska.Numpy.interpolate(dc[type, name], times_in, times_out,
+                                     method='linear')
     def _interp_times(dc):
-        return times
-    units = unit_table[type].get(name, '')
+        return times_out*apu.s
     add_derived_field(type, "interp_%s" % name, _interp, [(type, name)],
                       units, time_func=_interp_times,
                       display_name=get_display_name(type, name))

acispy/plots.py
@@ -2,7 +2,7 @@ from Ska.Matplotlib import plot_cxctime, pointpair
 from matplotlib import font_manager
 import matplotlib.pyplot as plt
 from matplotlib.dates import num2date
-from acispy.utils import state_labels, unit_labels, interpolate
+from acispy.utils import unit_labels, interpolate_indexes
 from Chandra.Time import DateTime
 from datetime import datetime
 from collections import OrderedDict
@@ -553,13 +553,13 @@ class PhasePlot(ACISPlot):
             times_in = dc.times(*y_field).value
             if x_src_name == "states":
                 tstart_out, tstop_out = dc.times(*x_field)
-                ok1, idxs1 = interpolate(times_in, tstart_out.value)
-                ok2, idxs2 = interpolate(times_in, tstop_out.value)
+                ok1, idxs1 = interpolate_indexes(times_in, tstart_out.value)
+                ok2, idxs2 = interpolate_indexes(times_in, tstop_out.value)
                 x = np.append(x[ok1], x[ok2])
                 y = np.append(y[idxs1], y[idxs2])
             else:
                 times_out = dc.times(*x_field).value
-                ok, idxs = interpolate(times_in, times_out)
+                ok, idxs = interpolate_indexes(times_in, times_out)
                 x = x[ok]
                 y = y[idxs]
         scp = ax.scatter(x, y)

acispy/utils.py
@@ -185,7 +185,7 @@ def get_display_name(type, name):
         display_name = name.upper()
     return display_name
 
-def interpolate(times_in, times_out):
+def interpolate_indexes(times_in, times_out):
     ok = (times_out >= times_in[0]) & (times_out <= times_in[-1])
     times_out = times_out[ok]
     idxs = Ska.Numpy.interpolate(np.arange(len(times_in)),
@@ -194,6 +194,5 @@ def interpolate(times_in, times_out):
     return ok, idxs
 
 def moving_average(a, n=5):
-    shape = a.shape[:-1] + (a.shape[-1] - n + 1, n)
-    strides = a.strides + (a.strides[-1],)
-    return np.mean(np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides), -1)
+    cumsum = np.cumsum(np.insert(a, 0, 0))
+    return (cumsum[n:]-cumsum[:-n])/n