5edcb631a9bb52c126f2392189e32780411fea6b
John ZuHone <jzuhone@gmail.com>
2016-05-11 12:09:30 -0400

    Bump version and make sure we can import ThermalModelRunner from the top

--- acispy/__init__.py
+++ acispy/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "0.1.4"
+__version__ = "0.2.0"
 
 from acispy.data_container import DataContainer
 from acispy.plots import DatePlot, MultiDatePlot, PhasePlot
-from acispy.thermal_models import SimulateCTIRun
+from acispy.thermal_models import SimulateCTIRun, ThermalModelRunner
