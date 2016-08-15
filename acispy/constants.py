0c7fa56b81d8335b425112a63d64e372b79f2231
John ZuHone <jzuhone@gmail.com>
2016-08-15 11:59:24 -0400

    Adding a constants module

---
+++ acispy/constants.py
@@ -0,0 +1,6 @@
+import astropy.units as u
+import numpy as np
+
+hrma_angular_diameter = 30*u.arcmin
+hrma_angular_radius = 0.5*hrma_angular_diameter
+hrma_angular_area = np.pi*hrma_angular_radius**2
