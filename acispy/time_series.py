c336ccbc879936ddc79ca2a7ae8b04d12d429a39
John ZuHone <jzuhone@gmail.com>
2017-03-29 12:38:14 -0400

    Add values and items to TimeSeriesData

--- acispy/time_series.py
+++ acispy/time_series.py
@@ -11,6 +11,13 @@ class TimeSeriesData(object):
     def keys(self):
         return list(self.table.keys())
 
+    def values(self):
+        return list(self.table.values())
+
+    def items(self):
+        return list(self.table.items())
+
+
 class EmptyTimeSeries(TimeSeriesData):
     def __init__(self):
         super(EmptyTimeSeries, self).__init__({})
