52cb1a73791ffcb1da66bd6439c771f44a7355f5
John ZuHone <jzuhone@gmail.com>
2018-05-11 13:33:26 -0400

    Bump version

--- acispy/__init__.py
+++ acispy/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.6.0"
+__version__ = "1.7.0"
 
 from acispy.dataset import ArchiveData, \
     TracelogData, EngineeringTracelogData, \
--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ setup(name='acispy',
       author_email='john.zuhone@cfa.harvard.edu',
       url='http://github.com/jzuhone/acispy',
       download_url='https://github.com/jzuhone/acispy/tarball/0.8.2',
-      install_requires=["six","requests","astropy"],
+      install_requires=["numpy>=1.13.1","six","requests","astropy"],
       scripts=scripts,
       classifiers=[
           'Intended Audience :: Science/Research',
           'Operating System :: OS Independent',
-          'Programming Language :: Python :: 2.7'
+          'Programming Language :: Python :: 3.6'
       ],
       )
