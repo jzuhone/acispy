28df5154a998b6f4dbcdb5d5e6b1665cccd60c26
John ZuHone <jzuhone@gmail.com>
2020-08-28 14:13:32 -0400

    We'll try to do this later when we have a better implementation

--- acispy/__init__.py
+++ acispy/__init__.py
@@ -12,6 +12,5 @@ from acispy.plots import DatePlot, MultiDatePlot, \
 from acispy.thermal_models import SimulateECSRun, \
     ThermalModelRunner, ThermalModelFromLoad, \
     ThermalModelFromRun, SimulateSingleObs, \
-    make_ecs_cmds
 from acispy.load_review import ACISLoadReview
 
